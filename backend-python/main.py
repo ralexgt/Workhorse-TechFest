5d7fe6330a4ceebda92efb9f7117dea1a3366113
Alex Roman <romanalexrares@gmail.com>
2025-09-09 21:16:49 +0300

    try everything

--- backend-python/main.py
+++ backend-python/main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from app.routes import app_routes
+from .app.routes import app_routes
 
 def create_app():
     app = Flask(__name__)
